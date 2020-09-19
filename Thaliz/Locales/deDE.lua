2a58359cf4a676e3d25dd637a99356339dad2d24
Bastien Gatellier <contact@bgatellier.fr>
2020-09-20 00:28:34 +0200

    feat: introduce spell localization in 8 locales

---
+++ Thaliz/Locales/deDE.lua
@@ -0,0 +1,8 @@
+local L = LibStub("AceLocale-3.0"):NewLocale("Thaliz", "deDE")
+if not L then return end
+
+-- Resurrection spell name
+L["Ancestral Spirit"] = "Geist der Ahnen"
+L["Rebirth"] = "Wiedergeburt"
+L["Redemption"] = "Erlösung"
+L["Resurrection"] = "Auferstehung"
