be0e3f047067b60d5d4a19407a78b1602c07d176
Bastien Gatellier <contact@bgatellier.fr>
2020-10-04 17:47:17 +0200

    Fix/missing french translation (#8)
    * fix: untranslated default message

--- Thaliz/Locales/frFR.lua
+++ Thaliz/Locales/frFR.lua
@@ -10,30 +10,30 @@ L["Resurrection"] = "Résurrection"
 --  Default resurrection messages
 L["Default Messages"] = {
 	-- UBRS
-	"(Ressing) CELA NE PEUT ÊTRE ! %s, occupez-vous de ces insectes !"									 ,	-- Lord Victor Nefarius
+	"(Résurrection) CELA NE PEUT ÊTRE ! %s, occupez-vous de ces insectes !"									 ,	-- Lord Victor Nefarius
 	-- ZG
-	"(Ressing) %s, je vous ai à l'œil !"										                         ,	-- Seigneur sanglant Mandokir
-	"(Ressing) %s, que ta RAGE m’envahisse !"                          									 ,	-- Grand prêtre Thekal
-	"(Ressing) Fuir ne vous servira à rien, %s !"										                 ,	-- Hakkar
+	"(Résurrection) %s, je vous ai à l'œil !"										                         ,	-- Seigneur sanglant Mandokir
+	"(Résurrection) %s, que ta RAGE m’envahisse !"                          									 ,	-- Grand prêtre Thekal
+	"(Résurrection) Fuir ne vous servira à rien, %s !"										                 ,	-- Hakkar
 	-- AQ20
-	"(Ressing) Maître %c %s, continuez le combat !"									                     ,	-- Général Rajaxx
+	"(Résurrection) Maître %c %s, continuez le combat !"									                     ,	-- Général Rajaxx
 	-- MC
-	"(Ressing) Vous avez sans doute besoin que je vous inflige quelques douleurs supplémentaires, %s !"	 ,	-- Chambellan Executus
-	"(Ressing) Top tôt, %s ! Tu es mort trop tôt !"									                     ,	-- Ragnaros
-	"(Ressing) Vous m'avez trahi, %s ! Justice sera faite, en vérité !"									 ,	-- Ragnaros
+	"(Résurrection) Vous avez sans doute besoin que je vous inflige quelques douleurs supplémentaires, %s !"	 ,	-- Chambellan Executus
+	"(Résurrection) Top tôt, %s ! Tu es mort trop tôt !"									                     ,	-- Ragnaros
+	"(Résurrection) Vous m'avez trahi, %s ! Justice sera faite, en vérité !"									 ,	-- Ragnaros
 	-- BWL
-	"(Ressing) Pardonnez-moi, %s ! Votre mort ne fait qu'ajouter à mon échec !"						     ,	-- Vaelastrasz le Corrompu
+	"(Résurrection) Pardonnez-moi, %s ! Votre mort ne fait qu'ajouter à mon échec !"						     ,	-- Vaelastrasz le Corrompu
 	-- AQ40
-	"(Ressing) Puisse votre mort servir d’exemple, %s !"										         ,	-- Le Prophète Skeram
-	"(Ressing) Seules la chair et les os. Les %cs sont des proies si faciles…"						     ,	-- Empereur Vek'lor
-	"(Ressing) Tes amis vont t'abandonner, %s."										                     ,	-- C'Thun
+	"(Résurrection) Puisse votre mort servir d’exemple, %s !"										         ,	-- Le Prophète Skeram
+	"(Résurrection) Seules la chair et les os. Les %cs sont des proies si faciles…"						     ,	-- Empereur Vek'lor
+	"(Résurrection) Tes amis vont t'abandonner, %s."										                     ,	-- C'Thun
 	-- Naxx
-	"(Ressing) Chut, %s… Ce sera bientôt fini."											                 ,	-- Anub'Rekhan
-	"(Ressing) Tuez %s au nom du maître !" 										                         ,	-- Grande veuve Faerlina
-	"(Ressing) Levez-vous, %s ! Levez-vous et combattez une fois encore !"			                     ,	-- Noth le Porte-peste
-	"(Ressing) Tu aurais dû rester chez toi, %s."									                     ,	-- Instructeur Razuvious
-	"(Ressing) La mort est la seule issue, %s !"									                     ,	-- Gothik le Moissonneur
-	"(Ressing) À moi la première résurrection ! Qui prend le pari ?"								     ,	-- Dame Blaumeux (Quatre Cavaliers)
-	"(Ressing) On joue plus, %s ?"									                                     ,	-- Le Recousu
-	"(Ressing) %s, vous arrivez trop tard… Je… dois… OBÉIR !"									         ,	-- Thaddius
+	"(Résurrection) Chut, %s… Ce sera bientôt fini."											                 ,	-- Anub'Rekhan
+	"(Résurrection) Tuez %s au nom du maître !" 										                         ,	-- Grande veuve Faerlina
+	"(Résurrection) Levez-vous, %s ! Levez-vous et combattez une fois encore !"			                     ,	-- Noth le Porte-peste
+	"(Résurrection) Tu aurais dû rester chez toi, %s."									                     ,	-- Instructeur Razuvious
+	"(Résurrection) La mort est la seule issue, %s !"									                     ,	-- Gothik le Moissonneur
+	"(Résurrection) À moi la première résurrection ! Qui prend le pari ?"								     ,	-- Dame Blaumeux (Quatre Cavaliers)
+	"(Résurrection) On joue plus, %s ?"									                                     ,	-- Le Recousu
+	"(Résurrection) %s, vous arrivez trop tard… Je… dois… OBÉIR !"									         ,	-- Thaddius
 }
