4808c4b1b88371c55faa8407473740eaafae4740
Bastien Gatellier <contact@bgatellier.fr>
2020-10-04 17:28:25 +0200

    Feature/translate default messages french (#7)
    * feature: add the french translation to the default resurrection messages

--- Thaliz/CHANGELOG.md
+++ Thaliz/CHANGELOG.md
@@ -7,6 +7,7 @@
   * You can now enable the private message (whisp) independently from the public ones (raid/party, say or yell)
   * The `/thaliz` command is now the only one available (`/thalizversion` and others concatenated commands have been removed).
   * The `/thaliz debug` command is now hidden from the UI and the CLI help, as it should be used by advanced users only
+* Added the french translation to the default resurrection messages
 * Removed the message displayed in chat when enabling/disabling the resurrection messages feature
 * Standardized the wording about the messages send when rezing someone: these are now only called "messages" (not macros or announcements)
 
--- Thaliz/Locales/enUS.lua
+++ Thaliz/Locales/enUS.lua
@@ -1,5 +1,6 @@
 local L = LibStub("AceLocale-3.0"):NewLocale("Thaliz", "enUS", true)
 
+
 -- Resurrection spell name
 L["Ancestral Spirit"] = "Ancestral Spirit"
 L["Rebirth"] = "Rebirth"
@@ -9,30 +10,30 @@ L["Resurrection"] = "Resurrection"
 --  Default resurrection messages
 L["Default Messages"] = {
 	-- UBRS
-	"(Ressing) THIS CANNOT BE!!! %s, deal with these insects."         ,	-- Rend Blackhand (UBRS)
+	"(Ressing) THIS CANNOT BE!!! %s, deal with these insects!"         ,	-- Lord Victor Nefarius
 	-- ZG
-	"(Ressing) I\'m keeping my eye on you, %s!"                        ,	-- Bloodlord Mandokir (Raptor boss)
-	"(Ressing) %s, fill me with your RAGE!"                            ,	-- High Priest Thekal (Tiger boss)
+	"(Ressing) %s! I'm watching you!"								   ,	-- Bloodlord Mandokir
+	"(Ressing) %s, fill me with your RAGE!"                            ,	-- High Priest Thekal
 	"(Ressing) Fleeing will do you no good, %s!"                       ,	-- Hakkar
 	-- AQ20
 	"(Ressing) Master %c %s, continue the fight!"                      ,	-- General Rajaxx
 	-- MC
 	"(Ressing) Perhaps you'll need another lesson in pain, %s!"        ,	-- Majordomo Executus
 	"(Ressing) Too soon, %s - you have died too soon!"                 ,	-- Ragnaros
-	"(Ressing) You have failed me, %s! Justice is met, indeed!"        , 	-- Ragnaros
+	"(Ressing) You have failed me, %s! Justice shall be met, indeed!"  ,	-- Ragnaros
 	-- BWL
-	"(Ressing) Forgive me %s, your death only adds to my failure."     ,	-- Vaelastrasz
+	"(Ressing) Forgive me %s! Your death only adds to my failure!"     ,	-- Vaelastrasz the Corrupt
 	-- AQ40
 	"(Ressing) Let your death serve as an example, %s!"                ,	-- Prophet Skeram
-	"(Ressing) Only flesh and bone. %cs are such easy prey, %s!"       ,	-- Emperor Vek'lor (Twins)
-	"(Ressing) Your friends will abandon you, %s!"                     ,	-- C'Thun
+	"(Ressing) Only flesh and bone. %cs are such easy prey, %s..."     ,	-- Emperor Vek'lor
+	"(Ressing) Your friends will abandon you, %s."                     ,	-- C'Thun
 	-- Naxx
 	"(Ressing) Shhh, %s... it will all be over soon."                  ,	-- Anub'Rekhan
 	"(Ressing) Slay %s in the masters name!"                           ,	-- Grand Widow Faerlina
 	"(Ressing) Rise, %s! Rise and fight once more!"                    ,	-- Noth the Plaguebringer
-	"(Ressing) You should have stayed home, %s!"                       ,	-- Instructor Razuvious
-	"(Ressing) Death is the only escape, %s."                          ,	-- Gothik the Harvester
-	"(Ressing) The first res goes to %s! Anyone care to wager?"        ,	-- Lady Blaumeux (4HM)
+	"(Ressing) You should have stayed home, %s."                       ,	-- Instructor Razuvious
+	"(Ressing) Death is the only escape, %s!"                          ,	-- Gothik the Harvester
+	"(Ressing) The first res goes to %s! Anyone care to wager?"        ,	-- Lady Blaumeux (Four Horsemen)
 	"(Ressing) No more play, %s?"                                      ,	-- Patchwerk
-	"(Ressing) %s, you are too late... I... must... OBEY!"             , 	-- Thaddius
+	"(Ressing) %s, you are too late... I... must... OBEY!"             ,	-- Thaddius
 }
