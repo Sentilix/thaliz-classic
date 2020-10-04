8c0536685c7d391215229c5f9d28f10551689041
Bastien Gatellier <contact@bgatellier.fr>
2020-10-04 17:20:48 +0200

    fix: default localized messages were not created (#6)

--- Thaliz/Locales/enUS.lua
+++ Thaliz/Locales/enUS.lua
@@ -9,30 +9,30 @@ L["Resurrection"] = "Resurrection"
 --  Default resurrection messages
 L["Default Messages"] = {
 	-- UBRS
-	{ "(Ressing) THIS CANNOT BE!!! %s, deal with these insects."         },	-- Rend Blackhand (UBRS)
+	"(Ressing) THIS CANNOT BE!!! %s, deal with these insects."         ,	-- Rend Blackhand (UBRS)
 	-- ZG
-	{ "(Ressing) I\'m keeping my eye on you, %s!"                        },	-- Bloodlord Mandokir (Raptor boss)
-	{ "(Ressing) %s, fill me with your RAGE!"                            },	-- High Priest Thekal (Tiger boss)
-	{ "(Ressing) Fleeing will do you no good, %s!"                       },	-- Hakkar
+	"(Ressing) I\'m keeping my eye on you, %s!"                        ,	-- Bloodlord Mandokir (Raptor boss)
+	"(Ressing) %s, fill me with your RAGE!"                            ,	-- High Priest Thekal (Tiger boss)
+	"(Ressing) Fleeing will do you no good, %s!"                       ,	-- Hakkar
 	-- AQ20
-	{ "(Ressing) Master %c %s, continue the fight!"                      },	-- General Rajaxx
+	"(Ressing) Master %c %s, continue the fight!"                      ,	-- General Rajaxx
 	-- MC
-	{ "(Ressing) Perhaps you'll need another lesson in pain, %s!"        },	-- Majordomo Executus
-	{ "(Ressing) Too soon, %s - you have died too soon!"                 },	-- Ragnaros
-	{ "(Ressing) You have failed me, %s! Justice is met, indeed!"        }, 	-- Ragnaros
+	"(Ressing) Perhaps you'll need another lesson in pain, %s!"        ,	-- Majordomo Executus
+	"(Ressing) Too soon, %s - you have died too soon!"                 ,	-- Ragnaros
+	"(Ressing) You have failed me, %s! Justice is met, indeed!"        , 	-- Ragnaros
 	-- BWL
-	{ "(Ressing) Forgive me %s, your death only adds to my failure."     },	-- Vaelastrasz
+	"(Ressing) Forgive me %s, your death only adds to my failure."     ,	-- Vaelastrasz
 	-- AQ40
-	{ "(Ressing) Let your death serve as an example, %s!"                },	-- Prophet Skeram
-	{ "(Ressing) Only flesh and bone. %cs are such easy prey, %s!"       },	-- Emperor Vek'lor (Twins)
-	{ "(Ressing) Your friends will abandon you, %s!"                     },	-- C'Thun
+	"(Ressing) Let your death serve as an example, %s!"                ,	-- Prophet Skeram
+	"(Ressing) Only flesh and bone. %cs are such easy prey, %s!"       ,	-- Emperor Vek'lor (Twins)
+	"(Ressing) Your friends will abandon you, %s!"                     ,	-- C'Thun
 	-- Naxx
-	{ "(Ressing) Shhh, %s... it will all be over soon."                  },	-- Anub'Rekhan
-	{ "(Ressing) Slay %s in the masters name!"                           },	-- Grand Widow Faerlina
-	{ "(Ressing) Rise, %s! Rise and fight once more!"                    },	-- Noth the Plaguebringer
-	{ "(Ressing) You should have stayed home, %s!"                       },	-- Instructor Razuvious
-	{ "(Ressing) Death is the only escape, %s."                          },	-- Gothik the Harvester
-	{ "(Ressing) The first res goes to %s! Anyone care to wager?"        },	-- Lady Blaumeux (4HM)
-	{ "(Ressing) No more play, %s?"                                      },	-- Patchwerk
-	{ "(Ressing) %s, you are too late... I... must... OBEY!"             } 	-- Thaddius
+	"(Ressing) Shhh, %s... it will all be over soon."                  ,	-- Anub'Rekhan
+	"(Ressing) Slay %s in the masters name!"                           ,	-- Grand Widow Faerlina
+	"(Ressing) Rise, %s! Rise and fight once more!"                    ,	-- Noth the Plaguebringer
+	"(Ressing) You should have stayed home, %s!"                       ,	-- Instructor Razuvious
+	"(Ressing) Death is the only escape, %s."                          ,	-- Gothik the Harvester
+	"(Ressing) The first res goes to %s! Anyone care to wager?"        ,	-- Lady Blaumeux (4HM)
+	"(Ressing) No more play, %s?"                                      ,	-- Patchwerk
+	"(Ressing) %s, you are too late... I... must... OBEY!"             , 	-- Thaddius
 }
